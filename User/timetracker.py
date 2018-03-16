7184e916012602108d8b829f633656e4c49e19c2
Robert Vock <robert.vock@4wdmedia.de>
2018-03-16 12:39:32 +0100

    improved writing of timetracker data

--- User/timetracker.py
+++ User/timetracker.py
@@ -4,16 +4,14 @@ import time
 
 class TimetrackerCommand(sublime_plugin.EventListener):
 	def on_post_save(self, view):
-		string = open('/Users/rvock/.timetracker', 'r').read()
 		saved_file = view.file_name()
 
 		if '4wdmedia/projekte' in saved_file:
 			project = saved_file.split('projekte/')[1].split('/')[0]
 			output = project + ":" + str(time.time()) + "\n"
-			string = string + output;
 
-			f = open('/Users/rvock/.timetracker', 'w')
-			f.write(string)
+			f = open('/Users/rvock/.timetracker', 'a+')
+			f.write(output)
 			f.close()
 
 			pass
